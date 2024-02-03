b47159e16ea89f6537740ab2f8ff1f27365f2661
juswaa101 <joshuayaacoub33@gmail.com>
2024-02-03 13:46:02 +0800

    rename label event modal

--- resources/views/components/modals/events/add-event-modal.blade.php
+++ resources/views/components/modals/events/add-event-modal.blade.php
@@ -4,7 +4,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Event Modal</h1>
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Event</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form id="addEventForm">
